e72bcccc0fc7ca34859f2843b4b2e4ad69b39967
KRITESH21 <kritesh21march@gmail.com>
2026-01-20 02:20:42 +0530

    implemented connection request recieved then accepted and rejected

--- src/app.js
+++ src/app.js
@@ -6,12 +6,14 @@ const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
 const {User} = require("./models/user");
+const userRouter = require("./routes/user");
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
+app.use("/", userRouter)
 
 connectDB()
   .then(() => {
